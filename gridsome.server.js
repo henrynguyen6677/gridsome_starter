66ebc7405239519444c32d4c39cd14bc3e3a0435
henrynguyen <henrynguyen6677@gmail.com>
2020-04-10 15:48:35 +0700

    Init commit

---
+++ gridsome.server.js
@@ -0,0 +1,22 @@
+// Server API makes it possible to hook into various parts of Gridsome
+// on server-side and add custom data to the GraphQL data layer.
+// Learn more: https://gridsome.org/docs/server-api/
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = function (api) {
+  api.loadSource(({ addCollection }) => {
+    // Use the Data store API here: https://gridsome.org/docs/data-store-api/
+  })
+
+  api.configureServer(app => {
+    app.get('/my-endpoint', (req, res) => {
+      res.send('Hello, world!')
+    })
+
+    app.post('/my-endpoint', (req, res) => {
+      res.send({ hello: 'grid some' })
+    })
+  })
+}
